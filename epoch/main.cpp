65855228d00e94e66e37db7d0f8c4e97efd33e56
climent jackey clement <cclement@volvocars.com>
2025-04-08 13:14:54 +0200

    added two time zones

--- epoch/main.cpp
+++ epoch/main.cpp
@@ -20,17 +20,30 @@ uint64_t GetEpochTimeFromString(const std::string& datetime)
         ss.ignore() >> milliseconds;
     }
 
-    auto tp = std::chrono::system_clock::from_time_t(std::mktime(&tm)) + std::chrono::milliseconds(static_cast<int64_t>(milliseconds));
+    // Parse timezone offset
+    if (ss >> sign >> tz_hour >> delimiter >> tz_min)
+    {
+        int offset = (tz_hour * 3600 + tz_min * 60) * (sign == '+' ? -1 : 1);
+        tm.tm_sec += offset; // Adjust seconds for timezone
+    }
+
+    std::time_t time = std::mktime(&tm);  // Local time representation
+    std::cout << "UTC time in seconds since epoch: " << time << '\n';
+
+    auto tp = std::chrono::system_clock::from_time_t(time) + std::chrono::milliseconds(static_cast<int64_t>(milliseconds));
+    // std::cout << "Secounds: " << std::chrono::duration_cast<std::chrono::milliseconds>(tp.time_since_epoch()).count() << '\n';
     return std::chrono::duration_cast<std::chrono::milliseconds>(tp.time_since_epoch()).count();
 }
 
 int main() {
     // Example input string
     std::string datetime = "2023-11-15T12:00:00.000+00:00";
+    std::cout << "Input datetime: " << datetime << std::endl;
     std::time_t epoch = GetEpochTimeFromString(datetime);
     std::cout << "Unix epoch time: " << epoch << std::endl;
-
+    std::cout << "----------------------------- " << std::endl;
     datetime = "2023-11-15T12:00:00.000+01:00";
+    std::cout << "Input datetime: " << datetime << std::endl;
     epoch = GetEpochTimeFromString(datetime);
     std::cout << "Unix epoch time: " << epoch << std::endl;
 
